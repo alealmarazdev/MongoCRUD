df6ad2533295c5d356aba03a91644815c531d225
alekirz <alejandra.almaraz23@gmail.com>
2019-03-02 00:22:44 -0600

    Todavia aprendiendo

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const indexRoutes = require('./routes/routes');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+// const routes = require('./routes/routes.js');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use (bodyParser.urlencoded({
+    extended: true
+}));
+
+app.listen(3000, () => (console.log('Servidor escuchando en el puerto 3000')));
+
+// app.use((req, resp, next) => {
+//     console.log('Servidor escuchando en');
+//     next();
+// }); 
+
+const db = require ('./config/keys').mongoURI
+
+mongoose.connect(db)
+.then(()=> console.log ('Conectado Mongo DB'))
+.catch(err=> console.log('Mongo error'))
+
+app.use('/', indexRoutes)
---
+++ src/config/keys.js
@@ -0,0 +1,3 @@
+module.exports= {
+    mongoURI:"mongodb://alej:Mra123@clasemongo-shard-00-00-y79nh.mongodb.net:27017,clasemongo-shard-00-01-y79nh.mongodb.net:27017,clasemongo-shard-00-02-y79nh.mongodb.net:27017/test?ssl=true&replicaSet=ClaseMongo-shard-0&authSource=admin&retryWrites=true"
+}
